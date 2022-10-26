5b066a27aeec2357a7ce64e6fc85ea1a1f1397ac
joshuajevon <joshuajevon05@gmail.com>
2022-10-26 21:00:33 +0700

    added component

--- coba.js
+++ coba.js
@@ -1,8 +1,17 @@
 let nama = "Rifian"
 
-console.log("Halo "+nama)
+console.log("Halodsgdsg "+nama)
 
 // const testing = (name) => console.log('hello, my name is ${name}')
 
 // module.exports = {testing}
 
+// const testing = require("@joshuajevon/testing123");
+
+// const app = testing();
+
+// app.get("/", (req, res) =>{
+//     res.send("Hello World");
+// })
+
+// app.listen(5000, () =>console.log("Listening to port 5000"));
---
+++ src/Button.jsx
@@ -0,0 +1,9 @@
+const Button = () => {
+    return (
+        <div className="Button">
+            <button>Test</button>
+        </div>
+    )
+}
+
+export default Button;
